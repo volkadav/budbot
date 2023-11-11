2043edc8ba56ff274d7ae1eec464b81aaa4efd7c
Michael Jackson <michael.o.jackson@gmail.com>
2023-11-11 23:19:18 +0000

    various dependency/build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.telegram:telegrambots:6.7.0")
+    implementation("org.telegram:telegrambots:6.8.0")
     implementation("org.projectlombok:lombok:1.18.30")
     implementation("com.google.guava:guava:32.1.1-jre")
     implementation("org.slf4j:slf4j-jdk14:2.0.7")
 
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
--- src/main/java/org/perilouscodpiece/budbot/BudBot.java
+++ src/main/java/org/perilouscodpiece/budbot/BudBot.java
@@ -14,6 +14,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 public class BudBot extends TelegramLongPollingBot {
